aaf737e2354b810191c7d02c0dfb89f7aee3581a
Akshat Dwivedi <akshatdwi755@gmail.com>
2025-02-14 23:13:18 +0530

    js

--- index.js
+++ index.js
@@ -1,22 +1,2 @@
-// .check property =  property that determines the checked state of an HTML checkbox or radio button element 
- 
-const myCheckbox = document.getElementById("myCheckbox")
-const visaBtn = document.getElementById("visaBtn")
-const mastercardBtn = document.getElementById("mastercardBtn")
-const paypalBtn = document.getElementById("paypalBtn")
-const subResult = document.getElementById("subResult")
-const paymentResult = document.getElementById("paymentResult")
-const mySubmit = document.getElementById("mySubmit")
-
-mySubmit.onclick=function(){
-if(myCheckbox.checked){
-    subResult.textContent=  ` you are subscribed`;
-
-}
-else{
-
-    subResult.textContent = ` youa are not subscribed`
-}
-}
-
+let username = window.prompt("Enter your username")
 
---
+++ methodchaning.js
@@ -0,0 +1,4 @@
+//Method chaining is a process of calling one method after one another in one continius line of code
+// Method chaining is used to reduce the number of lines of code
+
+let username =window.prompt("Enter your username:")
