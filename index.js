7098a51de99b6861579ad32b14a014f75e558b22
Akshat Dwivedi <akshatdwi755@gmail.com>
2025-02-15 11:10:57 +0530

    f
    vf

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+let loggedIn = false;
+let username;
+let password; 
 
-let username = "";
-while(username ===""){
-    username= window.prompt(` enter your suer name`)
- 
+while(!loggedIn){
+    username =window.prompt("Enter your username: ")
+    password = window.prompt("Enter your password: ")
+    if(username==="akshat" && password==="akshat21")
+        
+    {
+        loggedIn = true;
+     console.log("you are logged- in Bitch!")
+    }
+    else{
+        console.log("Invalid username or password")
+    }
 }
-console.log(`your username is${username}`) 
+
--- whileloop.js
+++ whileloop.js
@@ -1,6 +1,7 @@
 
-let username= window.prompt(" enter your suer name")
-while(username ===""){
-    console.log("you didn't enter your username")
-}
-console.log("your username is",username) 
+let username = "";
+do{
+    username= window.prompt(` enter your suer name`)
+ 
+}while(username ==="" || username === null)
+console.log(`your username is ${username}`) 
