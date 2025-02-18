cafb6926192aac0ec561a6ea915820d8f18a4b64
Akshat Dwivedi <akshatdwi755@gmail.com>
2025-02-18 23:55:12 +0530

    Update constructor.js

--- function declaration and expressions/constructor.js
+++ function declaration and expressions/constructor.js
@@ -8,3 +8,7 @@ function Car(make, model, year, color) {
 }
 
 let car1 = new Car("Toyota", "Camry", 2015, "Blue");
+let car2 = new Car("Honda", "Civic", 2018, "Red");
+let car3 = new Car("Ford", "Mustang", 2020, "Black");
+
+console.log(car3);
