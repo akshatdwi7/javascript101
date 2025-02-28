d43d4da16da3250e546a19ae678aae592d78ca78
Akshat Dwivedi <akshatdwi755@gmail.com>
2025-02-28 22:19:30 +0530

    js

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-const element = document.getElementById("fruits");
-const firstChild = element.firstElementChild;
-firstChild.style.backgroundColor = "red";
+const box = document.getElementById("box")
 
-const allFirst = document.querySelectorAll("ul");
-allFirst.forEach(ul => {
-    const firstLi = ul.firstElementChild;
-    if (firstLi) {
-        firstLi.style.backgroundColor = "red";
-    }
-});
 
+box.addEventListener("click",event=>{
+    event.target.style.backgroundColor="tomato"
+    event.target.textContent = "ouch 😭"
+
+})
